e61caf4897b989e0289898471fe3571fb3fa41e3
gladsonotavio <gladsonotavio@gmail.com>
2014-08-07 10:20:43 -0300

    comment

--- producaoIntelectual.php
+++ producaoIntelectual.php
@@ -4,7 +4,7 @@
 <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<link rel="stylesheet" type="text/css" href="css/lattes.css"  />
-	<title>Produção Intelectual de novo</title>
+	<title>Produção Intelectual</title>
 </head>
 
 <body>
